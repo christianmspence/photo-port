21ba11b0a8ad03e87975363561deabb9f654936f
christianmspence <103942566+christianmspence@users.noreply.github.com>
2022-09-08 17:37:29 -0400

    introduced hooks and props

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import coverImage from "../../assets/cover/cover-image.jpg";
+import { LoremIpsum } from 'react-lorem-ipsum';
 
 function About() {
     return (
         <section className="my-5">
             <h1 id="about">Who am I?</h1>
             <img src={coverImage} className="my-2" style={{ width: "100%" }} alt="cover" />
+            <p>
+                <LoremIpsum p={2} />
+            </p>
         </section>
     );
 }
