d8d76937a2ec3ba7d4a60810611fd79fce32b5d9
christianmspence <103942566+christianmspence@users.noreply.github.com>
2022-09-08 18:12:58 -0400

    tests written

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -7,9 +7,7 @@ function About() {
         <section className="my-5">
             <h1 id="about">Who am I?</h1>
             <img src={coverImage} className="my-2" style={{ width: "100%" }} alt="cover" />
-            <p>
                 <LoremIpsum p={2} />
-            </p>
         </section>
     );
 }
